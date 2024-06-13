f07449aa503982c531f93f01f1a9701caef2e561
CodeYouness <ylijassi@gmail.com>
2024-06-13 15:41:53 +0200

    added axios

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,7 +10,12 @@ export default {
 
 <template>
 
-
+<header>
+    <div>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/2/21/Yu-Gi-Oh%21.png" alt="yugioh logo">
+    </div>
+    <h1>Yu-Gi-Oh Api</h1>
+</header>
 
 </template>
 
@@ -18,4 +23,11 @@ export default {
 @use '../styles/partials/mixin' as *;
 @use '../styles/partials/variable' as *;
 
+header {
+    display: flex;
+    height: 10vh;
+    img {
+        width: 100%;
+    }
+}
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,31 @@
 <script>
 import MainCardList from './MainCardList.vue'
+import axios from 'axios';
 
 export default {
     data() {
         return {
+            cardList: []
         }
+    },
+    methods: {
+        getCardList(){
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+                .then(function (response) {
+                    // handle success
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+                .finally(function () {
+                    // always executed
+                });
+        }
+    },
+    created(){
+        this.getCardList
     },
     components: {
         MainCardList
