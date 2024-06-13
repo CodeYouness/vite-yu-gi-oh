9520e4e21aa3718064a2a5b979c40b98d6c9138d
CodeYouness <ylijassi@gmail.com>
2024-06-13 18:32:18 +0200

    added in mainsinglecard each card with props

--- src/components/MainCardList.vue
+++ src/components/MainCardList.vue
@@ -1,5 +1,5 @@
 <script>
-import MainSingleCard from './MainSingleCard.vue'
+import MainSingleCard from './MainSingleCard.vue';
 
 export default {
     data() {
@@ -21,15 +21,7 @@ export default {
 <template>
     <section class="container">
         <div class="row">
-            <div class="col-5th" v-for="(card,index) in cardList" :key="index">
-                <div class="card w-100">
-                    <img :src="card.card_images[0].image_url" class="card-img-top" :alt="card.name">
-                    <div class="card-body">
-                        <h2 class="card-title">{{ card.name }}</h2>
-                        <p class="card-text">{{ card.archetype }}</p>
-                    </div>
-                </div>
-            </div>
+            <MainSingleCard v-for="(card,index) in cardList" :key="index" :card="card"/>
         </div>
     </section>
 </template>
@@ -37,10 +29,5 @@ export default {
 <style lang="scss" scoped>
 @use '../styles/partials/mixin' as *;
 @use '../styles/partials/variable' as *;
-
-.col-5th {
-            flex: 0 0 20%;
-            max-width: 20%;
-        }
 
 </style>
--- src/components/MainSingleCard.vue
+++ src/components/MainSingleCard.vue
@@ -4,15 +4,34 @@ export default {
         return {
         }
     },
+    props: {
+        card: {
+            type: Object,
+            required: true
+        }
+    }
 }
 </script>
 
 <template>
-
+    <div class="col-5th">
+        <section class="card">
+            <img :src="card.card_images[0].image_url" class="card-img-top" :alt="card.name">
+            <div class="card-body">
+                <h2 class="card-title">{{ card.name }}</h2>
+                <p class="card-text">{{ card.archetype }}</p>
+            </div>
+        </section>
+    </div>
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/partials/mixin' as *;
 @use '../styles/partials/variable' as *;
+
+.col-5th {
+            flex: 0 0 20%;
+            max-width: 20%;
+        }
 
 </style>
