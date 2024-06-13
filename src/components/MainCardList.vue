5be73d1ef2622eb7278376d533bdb8ab2877d572
CodeYouness <ylijassi@gmail.com>
2024-06-13 15:08:10 +0200

    added vue components to app.vue

---
+++ src/components/MainCardList.vue
@@ -0,0 +1,20 @@
+<script>
+import MainSingleCard from './MainSingleCard.vue'
+
+export default {
+    data() {
+        return {
+        }
+    },
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/mixin' as *;
+@use '../styles/partials/variable' as *;
+
+</style>
