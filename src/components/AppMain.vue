30b148aae2eed015d2c51b14a4d546dd433ebe36
CodeYouness <ylijassi@gmail.com>
2024-06-14 16:28:09 +0200

    added list of archetype

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,11 +3,13 @@ import MainCardList from './MainCardList.vue'
 import axios from 'axios';
 import { store } from '../store';
 import MainLoader from './MainLoader.vue'
+import AppSearch from './AppSearch.vue'
 
 export default {
     components: {
         MainCardList,
-        MainLoader
+        MainLoader,
+        AppSearch
     },
     data() {
         return {
@@ -28,22 +30,39 @@ export default {
                 console.log(error);
             })
         },
+        getArchetypeList(){
+            axios.get('https://db.ygoprodeck.com/api/v7/archetypes.php')
+            .then((response) => {
+                // handle success
+                console.log(response.data);
+                this.store.archetypes = response.data
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+        },
         loadingTime(){
             setTimeout( () => {
                 this.isLoaded = true
             }, 3000)
+        },
+        consoletry(){
+            console.log('okay')
         }
         
     },
     created(){
         this.getCardList()
+        this.getArchetypeList()
         this.loadingTime()
     }
 }
 </script>
 
 <template>
-    <main class="bg-secondary py-3 overflow-y-scroll">
+    <main class="bg-secondary overflow-y-scroll">
+        <AppSearch v-if="isLoaded" @searched="consoletry"/>
         <MainCardList v-if="isLoaded"/>
         <MainLoader v-else/>
     </main>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,28 @@
+<script>
+import { store } from '../store';
+
+export default {
+    data() {
+        return {
+            store
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="container my-3">
+        <div class="row">
+            <div class="col-3">
+                <select class="form-select" aria-label="Default select example">
+                    <option selected>Select an archetype</option>
+                    <option v-for="(archetype,index) in store.archetypes" @click="$emit('searched')">{{store.archetype[index].archetype_name}}</option>
+                </select>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
